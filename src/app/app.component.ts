141766833f77d6049e20669372cf9d2931dc1044
Namito Yokota <namitoyokota@icloud.com>
2023-05-23 15:49:22 -0500

    csv parser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxCSVParserError, NgxCsvParser } from 'ngx-csv-parser';
 
 @Component({
     selector: 'app-root',
@@ -6,13 +7,32 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+    csvRecords: any;
+    header: boolean = false;
     file: File | null = null;
+
+    constructor(private ngxCsvParser: NgxCsvParser) {}
 
     onFileInput(files: FileList | null): void {
         console.log(files);
 
         if (files) {
             this.file = files.item(0);
+
+            this.header = (this.header as unknown as string) === 'true' || this.header === true;
+
+            this.ngxCsvParser
+                .parse(files[0], { header: this.header, delimiter: ',', encoding: 'utf8' })
+                .pipe()
+                .subscribe({
+                    next: (result): void => {
+                        console.log('Result', result);
+                        this.csvRecords = result;
+                    },
+                    error: (error: NgxCSVParserError): void => {
+                        console.log('Error', error);
+                    },
+                });
         }
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgxCsvParserModule } from 'ngx-csv-parser';
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule, NgxCsvParserModule],
     providers: [],
     bootstrap: [AppComponent],
 })
