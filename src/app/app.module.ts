1179b4967300e174e5f0f651ff240934ea688ed3
Namito Yokota <namitoyokota@icloud.com>
2023-05-23 20:45:12 -0500

    install material ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxCsvParserModule } from 'ngx-csv-parser';
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, NgxCsvParserModule],
+    imports: [BrowserModule, NgxCsvParserModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
