f701b2db4d2876cb7c6d14f3265b6068768c6c2f
Namito Yokota <namitoyokota@icloud.com>
2023-05-23 21:12:09 -0500

    ui cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CommonModule } from '@angular/common';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxCsvParserModule } from 'ngx-csv-parser';
@@ -8,7 +9,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, NgxCsvParserModule, BrowserAnimationsModule, MatExpansionModule],
+    imports: [BrowserModule, CommonModule, NgxCsvParserModule, BrowserAnimationsModule, MatExpansionModule],
     providers: [],
     bootstrap: [AppComponent],
 })
