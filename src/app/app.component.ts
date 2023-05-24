8ce101e4c8aa54538a6349e8214b488de58dd3be
Namito Yokota <namitoyokota@icloud.com>
2023-05-23 20:41:55 -0500

    check for duplicates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,10 @@ interface Transaction {
 })
 export class AppComponent {
     csvRecords: Transaction[] = [];
-    header: boolean = true;
+    header = true;
     file: File | null = null;
+
+    mp = new Map<string, Transaction[]>();
 
     constructor(private ngxCsvParser: NgxCsvParser) {}
 
@@ -30,8 +32,20 @@ export class AppComponent {
                 .pipe()
                 .subscribe({
                     next: (result): void => {
-                        console.log('Result', result);
                         this.csvRecords = result as Transaction[];
+
+                        const m = new Map<string, Transaction[]>();
+                        this.csvRecords.forEach((transaction) => {
+                            if (m.has(transaction.Description)) {
+                                const currentList = m.get(transaction.Description) as Transaction[];
+                                m.set(transaction.Description, [...currentList, transaction]);
+                            } else {
+                                m.set(transaction.Description, [transaction]);
+                            }
+                        });
+
+                        this.mp = new Map([...m].sort((a, b) => (a[1].length > b[1].length ? -1 : 1)));
+                        console.log(this.mp);
                     },
                     error: (error: NgxCSVParserError): void => {
                         console.log('Error', error);
