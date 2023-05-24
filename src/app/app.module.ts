9b26ebbadebb9779205bcabf6b1b1d0ae6a89c7e
Namito Yokota <namitoyokota@icloud.com>
2023-05-23 20:58:20 -0500

    display transactions with expansion component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatExpansionModule } from '@angular/material/expansion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxCsvParserModule } from 'ngx-csv-parser';
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, NgxCsvParserModule, BrowserAnimationsModule],
+    imports: [BrowserModule, NgxCsvParserModule, BrowserAnimationsModule, MatExpansionModule],
     providers: [],
     bootstrap: [AppComponent],
 })
